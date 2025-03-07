4e8d6fbf5eac422767af911e29303dc99f926a18
Emrah Vardar <emrahvardar26@gmail.com>
2025-03-07 14:01:37 +0300

    Ödevler yapıldı

---
+++ BodyMassIndex.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BodyMassIndex {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Please enter your height as meter");
+        double height = scanner.nextDouble();
+
+        System.out.println("Please enter your weight as kilogram");
+        double weight = scanner.nextDouble();
+
+        int bmi = (int) (weight / (height*height));
+
+        System.out.println("Your BMI is  " + bmi);
+
+    }}
---
+++ Hafta1/GroceryCheckOut.java
@@ -0,0 +1,31 @@
+package Hafta1;
+
+import java.util.Scanner;
+
+public class GroceryCheckOut {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+       System.out.println("How many kgs of pear?");
+       double pear = scanner.nextDouble();
+
+       System.out.println("How many kgs of apple?");
+       double apple = scanner.nextDouble();
+
+       System.out.println("How many kgs of tomatoes?");
+       double tomato = scanner.nextDouble();
+
+       System.out.println("How many kgs of bananas?");
+       double banana = scanner.nextDouble();
+
+       System.out.println("How many kgs of eggplant?");
+       double eggplant = scanner.nextDouble();
+
+       double totalprice = (pear * 2.14) + (apple * 3.67) + (tomato * 1.11) + (banana * 0.95) + (eggplant * 5);
+       System.out.println("Your total price is " + totalprice + "$");
+
+
+    }
+}
