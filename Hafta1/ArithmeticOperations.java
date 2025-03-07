032e8c8f8e5bf2bfb057ed3d1d6cb139282c1462
Emrah Vardar <emrahvardar26@gmail.com>
2025-03-07 17:03:01 +0300

    açıklamalar eklendi

---
+++ Hafta1/ArithmeticOperations.java
@@ -0,0 +1,26 @@
+package Hafta1;
+
+import java.util.Scanner;
+
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        //kullanıcıdan sayıları alıyoruz
+        System.out.println("enter a value for number a");
+        double numa = scanner.nextDouble();
+
+        System.out.println("enter a value for number b");
+        double numb = scanner.nextDouble();
+
+        System.out.println("enter a value for number c");
+        double numc = scanner.nextDouble();
+
+
+        //sonucu hesaplayıp çıktı veriyoruz
+        double result = numa+numb*numc-numb;
+        System.out.println("The result of " + numa + " + " + numb + " * " + numc + " - " + numb + " = " + result);
+
+    }
+}
