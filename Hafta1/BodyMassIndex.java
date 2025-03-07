6c95838680efcbd78493400dd9105e8ce965d7cb
Emrah Vardar <emrahvardar26@gmail.com>
2025-03-07 15:37:02 +0300

    plane ticket price eklendi

--- Hafta1/BodyMassIndex.java
+++ Hafta1/BodyMassIndex.java
@@ -5,17 +5,21 @@ import java.util.Scanner;
 public class BodyMassIndex {
     public static void main(String[] args) {
 
+
         Scanner scanner = new Scanner(System.in);
 
-
+        //kullanıcıdan boyu al
         System.out.println("Please enter your height as meter");
         double height = scanner.nextDouble();
 
+        //kullanıcıdan kiloyu al
         System.out.println("Please enter your weight as kilogram");
         double weight = scanner.nextDouble();
 
+        // vücut kitle endeksi = kilo / boy2
         int bmi = (int) (weight / (height*height));
 
+        //kullanıcıya çıktı ver
         System.out.println("Your BMI is  " + bmi);
 
     }}
