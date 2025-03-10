1ec8ac264ebd06451e89b94e68892c7182297742
Emrah Vardar <emrahvardar26@gmail.com>
2025-03-10 17:18:20 +0300

    açıklamalar eklendi

--- Hafta1/CastingExercise.java
+++ Hafta1/CastingExercise.java
@@ -5,18 +5,21 @@ import java.util.Scanner;
 public class CastingExercise {
     public static void main(String[] args) {
 
+
+        //Kullanıcıdan tam sayı al
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter an integer number: ");
         int intValue = scanner.nextInt();
 
-
+        // ondalıklı sayı al
         System.out.println("Enter a decimal number");
         double doubValue = scanner.nextDouble();
 
-
+        // casting işlemini yap
         double intToDouble = intValue;
         intValue =(int) doubValue;
 
+        //sonucu yazdır
         System.out.println("İnteger to decimal is= " + intToDouble);
         System.out.println("Decimal to integer is= " + intValue);
 
