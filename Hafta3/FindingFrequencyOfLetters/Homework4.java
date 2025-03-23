893fe8c0b11386026f9b836d5eefa97f6e47745b
Emrah Vardar <emrahvardar26@gmail.com>
2025-03-23 22:55:15 +0300

    açıklamalar eklendi

---
+++ Hafta3/FindingFrequencyOfLetters/Homework4.java
@@ -0,0 +1,17 @@
+package Hafta3.FindingFrequencyOfLetters;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Homework4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter word");
+        String word = scanner.nextLine().toLowerCase();
+
+
+        HashMap<String,Integer> frequencyMap = new HashMap<>();
+    }
+}
