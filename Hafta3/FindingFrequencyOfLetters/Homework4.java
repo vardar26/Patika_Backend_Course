d8645b0cb0c7eb7f76cf1dd812b2aa81897fea9e
Emrah Vardar <emrahvardar26@gmail.com>
2025-03-24 14:27:49 +0300

    açıklamalar eklendi

--- Hafta3/FindingFrequencyOfLetters/Homework4.java
+++ Hafta3/FindingFrequencyOfLetters/Homework4.java
@@ -1,17 +1,35 @@
 package Hafta3.FindingFrequencyOfLetters;
 
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Scanner;
+import java.util.*;
 
 public class Homework4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter word");
-        String word = scanner.nextLine().toLowerCase();
+        String word = scanner.nextLine().toLowerCase().replace(" ", "");;
 
 
-        HashMap<String,Integer> frequencyMap = new HashMap<>();
+        HashMap<Character,Integer> frequencyMap = new HashMap<>();
+
+        // ✅ Harfleri kontrol et
+        for (char ch : word.toCharArray()) {
+            if (frequencyMap.containsKey(ch)) {
+                // Eğer harf daha önce eklenmişse değeri artır
+                frequencyMap.put(ch, frequencyMap.get(ch) + 1);
+            } else {
+                // Eğer harf daha önce yoksa değeri 1 yap
+                frequencyMap.put(ch, 1);
+            }
+
+        }
+        System.out.println("\nHarf Frekansları:");
+        for (Map.Entry<Character, Integer> entry : frequencyMap.entrySet()) {
+            System.out.println(entry.getKey() + " => " + entry.getValue());
+        }
+
+        scanner.close();
+
     }
+
 }
