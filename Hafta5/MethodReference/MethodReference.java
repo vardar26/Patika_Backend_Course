6d109e5869539a4bc0acf9af3010445e5421c766
Emrah Vardar <emrahvardar26@gmail.com>
2025-04-09 13:29:10 +0300

    açıklamalar eklendi

---
+++ Hafta5/MethodReference/MethodReference.java
@@ -0,0 +1,18 @@
+package Hafta5.MethodReference;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MethodReference {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+
+        names.add("Ahmet");
+        names.add("Ayşe");
+        names.add("Mehmet");
+        names.add("Zeynep");
+
+        // ✅ Method reference kullanımı
+        names.forEach(System.out::println);
+    }
+}
